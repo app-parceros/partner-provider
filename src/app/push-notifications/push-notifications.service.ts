ed1bf1e388291ec66fee826af464125638cc2db0
Iván Ricardo <ir.taimal10@uniandes.edu.co>
2020-06-10 09:50:24 -0500

    impl:PUSH_NOTIFICATION, add condition to avoid load of notification in dev server

--- src/app/push-notifications/push-notifications.service.ts
+++ src/app/push-notifications/push-notifications.service.ts
@@ -3,9 +3,11 @@ import {
     Plugins,
     PushNotification,
     PushNotificationToken,
-    PushNotificationActionPerformed
+    PushNotificationActionPerformed, Capacitor
 } from '@capacitor/core';
 import {RestApiPlatformService} from '../rest-api/rest-api-platform.service';
+
+const isPushNotificationsAvailable = Capacitor.isPluginAvailable('PushNotifications');
 
 
 const {PushNotifications, Modals, Geolocation} = Plugins;
@@ -21,51 +23,52 @@ export class PushNotificationsService {
     }
 
     public initConfiguration() {
+        if (isPushNotificationsAvailable) { // Register with Apple / Google to receive push via APNS/FCM
+            PushNotifications.register();
 
-        // Register with Apple / Google to receive push via APNS/FCM
-        PushNotifications.register();
+            // On succcess, we should be able to receive notifications
+            PushNotifications.addListener('registration',
+                async (token: PushNotificationToken) => {
+                    alert('Push registration success, token: ' + token.value);
+                    console.log('Push registration success, token: ' + token.value);
+                    this.notificationToken = token;
+                    await this.sendRegistrationToServer(token);
+                }
+            );
 
-        // On succcess, we should be able to receive notifications
-        PushNotifications.addListener('registration',
-            async (token: PushNotificationToken) => {
-                alert('Push registration success, token: ' + token.value);
-                console.log('Push registration success, token: ' + token.value);
-                this.notificationToken = token;
-                await this.sendRegistrationToServer(token);
-            }
-        );
+            // Some issue with our setup and push will not work
+            PushNotifications.addListener('registrationError',
+                (error: any) => {
+                    alert('Error on registration: ' + JSON.stringify(error));
+                }
+            );
 
-        // Some issue with our setup and push will not work
-        PushNotifications.addListener('registrationError',
-            (error: any) => {
-                alert('Error on registration: ' + JSON.stringify(error));
-            }
-        );
+            // Show us the notification payload if the app is open on our device
+            PushNotifications.addListener('pushNotificationReceived',
+                (notification: PushNotification) => {
+                    const audio1 = new Audio('assets/audio.mp3');
+                    console.log('Audio');
+                    audio1.play();
+                    // alert('Push received: ' + JSON.stringify(notification));
+                    console.log('Push received: ', notification);
 
-        // Show us the notification payload if the app is open on our device
-        PushNotifications.addListener('pushNotificationReceived',
-            (notification: PushNotification) => {
-                const audio1 = new Audio('assets/audio.mp3');
-                console.log('Audio');
-                audio1.play();
-                // alert('Push received: ' + JSON.stringify(notification));
-                console.log('Push received: ', notification);
+                    const alertRet = Modals.alert({
+                        title: notification.title,
+                        message: notification.body
+                    });
 
-                const alertRet = Modals.alert({
-                    title: notification.title,
-                    message: notification.body
-                });
+                }
+            );
 
-            }
-        );
+            // Method called when tapping on a notification
+            PushNotifications.addListener('pushNotificationActionPerformed',
+                (notification: PushNotificationActionPerformed) => {
+                    alert('Push action performed: ' + JSON.stringify(notification));
+                    console.log('Push action performed: ' + notification);
+                }
+            );
+        }
 
-        // Method called when tapping on a notification
-        PushNotifications.addListener('pushNotificationActionPerformed',
-            (notification: PushNotificationActionPerformed) => {
-                alert('Push action performed: ' + JSON.stringify(notification));
-                console.log('Push action performed: ' + notification);
-            }
-        );
     }
 
     public async sendRegistrationToServer(token: PushNotificationToken) {
